7e198f7afaa606573f7708b5f37c2194e3a194e0
CHITTIMON0 <85325231+CHITTIMON0@users.noreply.github.com>
2021-06-04 08:48:38 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,16 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = 🍿༺ ──•◈•─⚜─•◈•──༻🍿
-Group🔻
-          @srsuggestionsofficial
-
-Movies🔻
-          @srsuggestionsmc
-
-Series🔻
-          @srsuggestionsseries
-🎞༺ ──•◈•─⚜─•◈•──༻🎞,
+                caption = @srsuggestionsmc,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
